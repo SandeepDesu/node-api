9c4f9c07b1ae8ef8abff5c65ef72279b4486ab99
Sandy <dsandy.ip@gmail.com>
2018-05-10 11:27:09 -0700

    https server

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 var express = require('express'),
     app = express();
+var https = require('https');
+var fs = require('fs');
+var key = fs.readFileSync('./key.pem');
+var cert = fs.readFileSync('./cert.pem')
+var https_options = {
+    key: key,
+    cert: cert
+};
 
+var PORT = 8000;
+var HOST = 'localhost';
 
+var server = https.createServer(https_options, app).listen(PORT, HOST);
 app.get("/", function (req, res) {
     res.send("welcome to docker api");
 })
-app.listen(8080);
+
+
+//app.listen(8080);
