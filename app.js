4a3dfcd49fbbf90e4464372b2eae03e13a381221
sandy <dsandy.ip@gmail.com>
2018-10-25 05:56:37 -0700

    updated cros origin request

--- app.js
+++ app.js
@@ -2,19 +2,13 @@ var express = require('express'),
     app = express(),
     bodyParser = require('body-parser'),
     mongoose = require('mongoose');
-    mongoose.connect('mongodb://sandy:Excellence@localhost:27017/sample');
-
-    app.use(bodyParser.json());
-
-    app.use('/v1',require('./routes'));
-
-    app.use(function(req,res,next){
-        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization,x-access-token");
-        next();
-    });
-
-
-
-
-
-    app.listen(8000);
+mongoose.connect('mongodb://sandy:Excellence@localhost:27017/sample');
+app.use(bodyParser.json());
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS, HEAD");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    next();
+});
+app.use('/v1', require('./routes'));
+app.listen(8000);
